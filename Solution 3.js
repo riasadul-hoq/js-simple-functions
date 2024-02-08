6f7abc0ebea3ac9ee6348fb3835640305a95c021
Riasadul Hoq <shaon777@gmail.com>
2024-02-08 01:27:35 +0000

    Comments added to Solution 3

--- Solution 3.js
+++ Solution 3.js
@@ -1,16 +1,21 @@
+/* Function deleteInvalids() that takes an array as input */
 function deleteInvalids(array) {
+  // Condition to check input data type - array
   if (Array.isArray(array)) {
+    // Array filter method to keep only number (excluding Nan) data type;
     let numbersOnly = array.filter((arr) => {
       return typeof arr === "number" && !isNaN(arr);
     });
     return numbersOnly;
+    // Other data type returns error
   } else {
     return "Invalid Array";
   }
 }
 
+// Function Calls
 console.log(
-  13,
+  18,
   deleteInvalids([
     1,
     null,
@@ -24,6 +29,6 @@ console.log(
   ])
 );
 
-console.log(27, deleteInvalids(["1", { num: 2 }, NaN]));
-console.log(28, deleteInvalids([1, 2, -3]));
-console.log(29, deleteInvalids({ num: [1, 2, 3] }));
+console.log(32, deleteInvalids(["1", { num: 2 }, NaN]));
+console.log(33, deleteInvalids([1, 2, -3]));
+console.log(34, deleteInvalids({ num: [1, 2, 3] }));
